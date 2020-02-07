f5491aafe25f5748421d8b87067abedb1cff5070
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-02-07 22:23:30 +0530

    chore: Update deps

---
+++ src/components/ShellBar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ShellBar extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Shell Bar
+            </div>
+        );
+    }
+}
+
+export default ShellBar
